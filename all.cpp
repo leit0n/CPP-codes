00bd1fe0097551fdc9b47799a60afc0398872cd2
mmleitao <marti.leitao@gmailcom>
2023-11-03 10:35:12 -0100

    ola

--- all.cpp
+++ all.cpp
@@ -240,3 +240,48 @@ using namespace std;
     return 0;
  }
  
+
+ #include <iostream>
+using namespace std;
+
+int myArray [] = { 1, 2 , 3, 4, 5 };
+
+
+int main(){
+    
+    cout << myArray[4] << endl;
+    cout << sizeof(myArray)/4 << endl;
+
+    return 0;
+}
+
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int myArray [] = { 1, 2 , 3, 4, 5 };
+
+string letras [2] [4];  //array bidimencional
+
+string letras2[2] [4] = {
+    {"A", "B", "C", "D"},
+    {"E", "F", "G", "H"}
+};
+
+
+int main(){
+    
+    for (int i =0; i < 2; i++){
+     for (int j =0; i < 4; j++){
+        cout << letras2[i][j] << endl;
+        }
+    }
+
+
+
+    return 0;
+}
+
